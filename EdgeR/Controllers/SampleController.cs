1a27ff91c196918593e0dff43a4b609c07730bb4
atif <atif_r@outlook.com>
2019-01-06 16:48:39 +0900

    Add project files.

---
+++ EdgeR/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+
+namespace EdgeR.Controllers
+{
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> RandomCompute()
+        {
+            Random random = new Random();
+            int miliseconds = random.Next(100, 1500);
+            Thread.Sleep(miliseconds);
+            return new string[] { "Processing Time: "+miliseconds };
+        }
+    }
+}
